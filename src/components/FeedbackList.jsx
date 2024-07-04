0033eec8aff0c08aaed68af534c0bb83ab37921d
ChrisLikesCoding <sharpchr@gmail.com>
2024-07-04 12:23:53 +0100

    this is the version with HTTP requests

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,17 +1,20 @@
-import { motion, AnimatePresence } from 'framer-motion';
-import { useContext } from 'react';
-import React from 'react';
-import FeedbackItem from './FeedbackItem';
-import FeedbackContext from '../context/FeedbackContext';
+import { motion, AnimatePresence } from 'framer-motion'
+import Spinner from './shared/Spinner'
+import { useContext } from 'react'
+import React from 'react'
+import FeedbackItem from './FeedbackItem'
+import FeedbackContext from '../context/FeedbackContext'
 
 function FeedbackList() {
-  const { feedback } = useContext(FeedbackContext);
+  const { feedback, isLoading } = useContext(FeedbackContext)
 
-  if (!feedback || feedback.length === 0) {
-    return <p>No feedback yet</p>;
+  if (!isLoading && (!feedback || feedback.length === 0)) {
+    return <p>No feedback yet</p>
   }
-  return (
-    <div className="feedback-list">
+  return isLoading ? (
+    <Spinner />
+  ) : (
+    <div className='feedback-list'>
       <AnimatePresence>
         {feedback.map((item) => (
           <motion.div
@@ -25,7 +28,7 @@ function FeedbackList() {
         ))}
       </AnimatePresence>
     </div>
-  );
+  )
 }
 
-export default FeedbackList;
+export default FeedbackList
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,48 +1,58 @@
-import { v4 as uuidv4 } from 'uuid'
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: 'This item is feedback item 1',
-      rating: 10,
-    },
-    {
-      id: 2,
-      text: 'This item is feedback item  2',
-      rating: 9,
-    },
-    {
-      id: 3,
-      text: 'This item is feedback item 3',
-      rating: 8,
-    },
-  ])
-
+  const [isLoading, setIsloading] = useState(true)
+  const [feedback, setFeedback] = useState([])
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   })
-  // set item to be updated  add feedback
+  useEffect(() => {
+    fetchFeedback()
+  }, [])
+  // fetch feedback
+  const fetchFeedback = async () => {
+    const response = await fetch(`/feedback?_sort=id&_order=desc`)
+    const data = await response.json()
+    console.log(data)
+
+    setFeedback(data)
+    setIsloading(false)
+  }
 
   // add feedback
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback, ...feedback])
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch('/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newFeedback),
+    })
+    const data = await response.json()
+    setFeedback([data, ...feedback])
   }
   // delete feedback
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
+      await fetch(`/feedback/${id}`, { method: 'DELETE' })
       setFeedback(feedback.filter((item) => item.id !== id))
     }
   }
   //update feedback item
-  const updateFeedback = (id, updItem) => {
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updItem),
+    })
+    const data = await response.json()
     setFeedback(
-      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+      feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
     )
     setFeedbackEdit({
       item: {},
@@ -61,6 +71,7 @@ export const FeedbackProvider = ({ children }) => {
       value={{
         feedback,
         feedbackEdit,
+        isLoading,
         deleteFeedback,
         addFeedback,
         editFeedback,
